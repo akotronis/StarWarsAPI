06001e5a4501c73185064d3f1674be4f4eb8e65e
akotronis <akotronis@gmail.com>
2025-09-04 18:06:42 +0300

    Register app urls

---
+++ app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from . import views
+
+
+app_name = 'app'
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register('films', views.FilmViewSet, basename='films')
+router.register('characters', views.CharacterViewSet, basename='characters')
+router.register('starships', views.StarshipViewSet, basename='starships')
+router.register('swapi-fetch-populate', views.SWAPIFetchPopulateView, basename='swapi-fetch-populate')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
