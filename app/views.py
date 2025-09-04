44f29303f77ff1a18d1422c69bc365ab787a7019
akotronis <akotronis@gmail.com>
2025-09-04 18:05:44 +0300

    Add views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
-# Create your views here.
+from . import mixins
+from . import models
+from . import serializers
+
+
+class FilmViewSet(mixins.CommonFunctionalityViewsetMixin, viewsets.ModelViewSet):
+    """
+    View for Film resource.
+    Filter by:
+    - "title" with "contains=<string>"
+    - page number with "page=<number>"
+    """
+    serializer_class = serializers.FilmSerializer
+    queryset = models.Film.objects.all()
+    filter_field = 'title'
+
+
+class CharacterViewSet(mixins.CommonFunctionalityViewsetMixin, viewsets.ModelViewSet):
+    """
+    View for Character resource.
+    Filter by:
+    - "name" with "contains=<string>"
+    - page number with "page=<number>"
+    """
+    serializer_class = serializers.CharacterSerializer
+    queryset = models.Character.objects.all()
+
+
+class StarshipViewSet(mixins.CommonFunctionalityViewsetMixin, viewsets.ModelViewSet):
+    """
+    View for Starship resource.
+    Filter by:
+    - "name" with "contains=<string>"
+    - page number with "page=<number>"
+    """
+    serializer_class = serializers.StarshipSerializer
+    queryset = models.Starship.objects.all()
+
+
+# By using ViewSet, this can be registered via router and appear in the DRF Browser API root view
+class SWAPIFetchPopulateView(viewsets.ViewSet):
+    """
+    Fetch resources "films, characters, starships" from SWAPI and populate database
+    """
+    def list(self, request):
+        return Response({'test': 1})
