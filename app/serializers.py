f944d5c1ec95dec910c5b4228957d16eb5e6b93f
akotronis <akotronis@gmail.com>
2025-09-05 18:09:54 +0300

    Modify serializers to reflect model changes

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
+from . import mixins
 from . import models
-
 
 class FilmSerializer(serializers.ModelSerializer):
     characters = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
@@ -31,21 +31,28 @@ class SWAPIFilmSerializer(serializers.Serializer):
     director = serializers.CharField(required=False)
     release_date = serializers.DateField(required=False)
     created = serializers.DateTimeField(required=False)
+    url = serializers.URLField(required=False, source='swapi_url')
     starships = serializers.ListField(child=serializers.URLField(), required=False)
 
 
-class SWAPICharacterSerializer(serializers.Serializer):
+class SWAPICharacterSerializer(mixins.HandleNumericsSerializerMixin, serializers.Serializer):
+    numeric_fields_as_strings = ['height'] 
+
     name = serializers.CharField(required=False)
-    height = serializers.FloatField(required=False)
+    height = serializers.FloatField(required=False, allow_null=True)
     gender = serializers.CharField(required=False)
     created = serializers.DateTimeField(required=False)
+    url = serializers.URLField(required=False, source='swapi_url')
     films = serializers.ListField(child=serializers.URLField(), required=False)
 
 
-class SWAPIStarshipSerializer(serializers.Serializer):
+class SWAPIStarshipSerializer(mixins.HandleNumericsSerializerMixin, serializers.Serializer):
+    numeric_fields_as_strings = ['cost_in_credits', 'hyperdrive_rating'] 
+
     name = serializers.CharField(required=False)
     model = serializers.CharField(required=False)
-    cost_in_credits = serializers.IntegerField(required=False)
-    hyperdrive_rating = serializers.FloatField(required=False)
+    cost_in_credits = serializers.IntegerField(required=False, allow_null=True)
+    hyperdrive_rating = serializers.FloatField(required=False, allow_null=True)
     created = serializers.DateTimeField(required=False)
-    character = serializers.URLField(required=False)
+    url = serializers.URLField(required=False, source='swapi_url')
+    pilots = serializers.ListField(child=serializers.URLField(), required=False, source='characters')
