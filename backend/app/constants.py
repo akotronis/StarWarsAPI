e5bd1f7faee8d287590f6bee318ef6f9a214fe2a
akotronis <akotronis@wings-ict-solutions.eu>
2025-09-26 09:42:04 +0000

    Modify services constants

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -1,4 +1,22 @@
-SWAPI_BASE_URL = "https://swapi.info/api"
+from django.db import models
+
+class ResourceEnum(models.TextChoices):
+    FILM = "film"
+    CHARACTER = "character"
+    STARSHIP = "starship"
+
+    @property
+    def plural(self):
+        return f"{self.value}s"
+    
+
+# SWAPI_BASE_URL = "https://swapi.info/api"
+SWAPI_BASE_URL = "http://srv-sw-mock:8080"
 SWAPI_FILMS_URL = f"{SWAPI_BASE_URL}/films"
 SWAPI_CHARACTERS_URL = f"{SWAPI_BASE_URL}/people"
 SWAPI_STARSHIPS_URL = f"{SWAPI_BASE_URL}/starships"
+
+TOTAL_FILMS = 1_000_000
+TOTAL_CHARACTERS = 5_000_000
+TOTAL_STARSHIPS = 2_000_000
+PAGE_SIZE = 1000
